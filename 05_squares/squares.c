9a3e5bb903ad93f5bad6b6e07995a771623dff26
Coursera Learner <nobody@nowhere.nul>
2020-10-01 21:32:10 +0000

    Squares prog

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -7,6 +7,10 @@
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
 int isInRange(int coord, int offset, int size) {
+  if ((coord >= offset) && (coord < offset + size))
+    {
+      return 1;
+    }
   // if coord is in range, return 1
   // else, return 0
   return 0;
@@ -17,28 +21,61 @@ int isInRange(int coord, int offset, int size) {
  * offset + size
  */
 int isAtBorder(int coord, int offset, int size) {
-  // if coord is equal to offest or offset + size
-  // return 1, else return 0
+  if ((coord == offset) || (coord == offset + size))
+    {  // if coord is equal to offest or offset + size
+      return 1;
+    }
+ // return 1, else return 0
   return 0;
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
+        //compute the max of size1 and (x_offset + size2).  Call this w
+  int w, h;
+  if (size1 >(x_offset + size2))
+    {
+      w = size1;
+    }
+  else w = x_offset + size2;
 
+  if (size1 > (y_offset + size2))
+    {
+      h = size1;
+    }
+  else h = y_offset +size2;
   //compute the max of size1 and (y_offset + size2).  Call this h
 
-  //count from 0 to h. Call the number you count with y
+  for (int y = 0; y <  h; y++)
+    {  //count from 0 to h. Call the number you count with y
 
-    //count from 0 to w. Call the number you count with x
+    for (int x = 0; x <  w; x++){    //count from 0 to w. Call the number you count with x
 
-      //check if  EITHER
+      int x_r1 = isInRange(x, x_offset, size2);
+      int y_r1 = isAtBorder(y, y_offset, size2 - 1);
+      int x_r2 = isAtBorder(x, x_offset, size2 - 1);
+      int y_r2 = isInRange(y, y_offset, size2);
+
+      if(x_r1 && y_r1 || y_r2 && x_r2)
+	{
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
-      // if so, print a *
-
+              // if so, print a *
+	printf("*");
+      }
+      else{
+	if ((x < size1) && ((y == 0) || (y == size1 - 1)) || ((y < size1) && ((x == 0) || (x == size1 - 1)))){
+	  printf("#");
+	}
+	else{
+	  printf(" ");
+	}
+      }
+    }
+    printf("\n");
+  }
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
